b11e3e3e5c82fb5993f5b425dd1e03523d5d5542
Benton Lake <benton@bentonlake.dev>
2023-03-26 17:16:29 -0400

    Fix Footer cy

--- apps/storybook-e2e/ui-react-webstd-e2e/src/e2e/footer/footer.cy.ts
+++ apps/storybook-e2e/ui-react-webstd-e2e/src/e2e/footer/footer.cy.ts
@@ -1,7 +1,7 @@
 describe('flameui-ui-react-webstd: Footer component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=footer--primary'));
-    
+
     it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Footer!');
+      cy.get('h1').should('contain', 'I\'m a footer!');
     });
 });
