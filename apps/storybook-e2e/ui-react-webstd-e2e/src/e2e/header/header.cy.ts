1d9edb9ab25aa3e19377e3aaae391a0f7b9a82be
Benton Lake <benton@bentonlake.dev>
2023-03-26 18:57:01 -0400

    Remove Header component from FlameUI

--- apps/storybook-e2e/ui-react-webstd-e2e/src/e2e/header/header.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('flameui-ui-react-webstd: Header component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=header--primary'));
-
-  it('should render the component', () => {
-    cy.get('h1').should('contain', 'Hello, World!');
-  });
-});
