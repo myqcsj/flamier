c61ca494e23209a3310e783fcc800fa70d1fa71c
Benton Lake <benton@bentonlake.dev>
2023-03-26 14:49:39 -0400

    Migrate to Storybook rc version

--- libs/flameui/ui-react-webstd/.storybook/main.js
+++ libs/flameui/ui-react-webstd/.storybook/main.js
@@ -1,19 +1,26 @@
-const { mergeConfig } = require('vite');
+const {
+  mergeConfig
+} = require('vite');
 const viteTsConfigPaths = require('vite-tsconfig-paths').default;
-
 module.exports = {
-  core: { builder: '@storybook/builder-vite' },
-  stories: ['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
-  async viteFinal(config, { configType }) {
+  stories: ['../src/lib/**/*.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  async viteFinal(config, {
+    configType
+  }) {
     return mergeConfig(config, {
-      plugins: [
-        viteTsConfigPaths({
-          root: '../../../../',
-        }),
-      ],
+      plugins: [viteTsConfigPaths({
+        root: '../../../../'
+      })]
     });
   },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
 
 // To customize your Vite configuration you can use the viteFinal field.
