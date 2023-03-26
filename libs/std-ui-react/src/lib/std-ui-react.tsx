5770a02007067f9553fc12945b70671478382088
Benton Lake <benton@bentonlake.dev>
2023-03-26 10:39:02 -0400

    Created std-ui-react, the std-ui library for react apps

---
+++ libs/std-ui-react/src/lib/std-ui-react.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface StdUiReactProps {}
+
+export function StdUiReact(props: StdUiReactProps) {
+  return (
+    <div>
+      <h1>Welcome to StdUiReact!</h1>
+    </div>
+  );
+}
+
+export default StdUiReact;
