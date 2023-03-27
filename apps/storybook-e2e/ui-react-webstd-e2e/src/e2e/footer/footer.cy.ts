673ed9793b02ce3685fe9fb618b7cdb65ccf5f39
Benton Lake <benton@bentonlake.dev>
2023-03-26 20:00:56 -0400

    Fully removed Footer component from FlameUI

--- apps/storybook-e2e/ui-react-webstd-e2e/src/e2e/footer/footer.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('flameui-ui-react-webstd: Footer component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=footer--primary'));
-
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'I\'m a footer!');
-    });
-});
